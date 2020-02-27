b9129842b6ed6a802374acea3e3ac0ddc876116e
moon9ua <koom6@naver.com>
2020-02-28 05:20:53 +0900

    get_next_line?

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmoon <gmoon@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/28 04:50:26 by gmoon             #+#    #+#             */
+/*   Updated: 2020/02/28 04:50:26 by gmoon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE
+
+int get_next_line(int fd, char **line);
+
+#endif
