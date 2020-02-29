ca347dd95292ec3b14579f807125376e57ebfab6
moon9ua <koom6@naver.com>
2020-02-29 09:49:44 +0900

    zzz

--- get_next_line/get_next_line_1.c
+++ get_next_line/get_next_line_1.c
@@ -14,6 +14,7 @@
 #include <stdio.h>
 #include <fcntl.h> // open, O_RDONLY
 
+// 문제 자체를 잘못 접근. 버퍼사이즈를 3정도로 생각하고 내일 다시!!!
 
 int is_line(char *buf)
 {
@@ -46,38 +47,71 @@ char *ft_cut(char *buf)
 {
 	char *ret;
 	int i;
-	static int flag;
-
-
+	
 	i = 0;
-	while (buf[i] != '\n' && buf[i] != '\0')
+	while (buf[i] != '\0' && buf[i] != '\n')
 		i++;
-	if (buf[i] == '\n')
-	{
-		ret = (char *)malloc(i + 1);
-		ft_memcpy(ret, buf, i);
-		ret[i] = '\0';
-	}
-	if (buf[i] == '\0')
-	{
-		ret = (char *)malloc(i + 1);
-		ft_memcpy(ret, buf, i + 1);
-		flag = 1;
-	}
+	// if (buf[i] == '\n')
+	// {
+	// 	ret = (char *)malloc(i + 1);
+	// 	ft_memcpy(ret, buf, i);
+	// 	ret[i] = '\0';
+	// 	return (ret);
+	// }
+	// if (buf[i] == '\0')
+	// {
+	// 	ret = (char *)malloc(i + 1);
+	// 	ft_memcpy(ret, buf, i);
+	// 	ret[i] = '\0';
+	// 	return (ret);
+	// }
+	ret = (char *)malloc(i + 1);
+	ft_memcpy(ret, buf, i);
+	ret[i] = '\0';
 	return (ret);
 }
 
 // int main()
 // {
-// 	char *buf1 = "moongua test\nhelloworld\n ' ' \nhihi";
+// 	char *buf1 = "second line\n";
 // 	printf("[%s]\n", ft_cut(buf1));
 // }
+
+int ft_len(char *str)
+{
+	int i;
+	i = 0;
+	while (str[i] != '\n' || str[i] != '\0')
+		i++;
+	return i;
+}
+
+int is_meet_n(char *str)
+{
+	while (1)
+	{
+		if (*str == '\n')
+			return 1;
+		if (*str == '\0')
+			return 0;
+		str++;
+	}
+}
+
+// int main()
+// {
+// 	char *a = "aaa";
+// 	char *b = "bbb\n";
+// 	printf("%d\n", is_meet_n(a));
+// 	printf("%d\n", is_meet_n(b));
+// }
+
+int line_count 
 
 int get_next_line(int fd, char **line)
 {
 	char *buf;
 	char *tmp;
-	static int flag;
 
 	buf = (char *)malloc(BUFFER_SIZE);
 	if (!buf)
@@ -89,33 +123,35 @@ int get_next_line(int fd, char **line)
 	char *tmp_buf;
 	tmp_buf = buf;
 
-	read(fd, buf, BUFFER_SIZE);
-	// printf("[%s]\n", buf);
-	// printf("%s\n", ft_cut(buf));
-	tmp = ft_cut(buf);
-	line[0] = tmp;
-	// printf("%s\n", tmp);
-	// printf("%s\n", line[0]);
-	// [%s]를 하며 왜 ]moongua test로 나오며, 왜 가끔은 아예 안나오는 것일까!!!
-	// 뭔가 메모리에 남아있어야만 가능한... 뭔가 내가 모르는 중요한 내용이 있나본데... ㅠㅠ...
-	tmp_buf = tmp_buf + ft_strlen(line[0]) + 1;
-
-	tmp = ft_cut(tmp_buf);
-	line[1] = tmp;
-	printf("%s\n", line[1]);
-
-
 	// read(fd, buf, BUFFER_SIZE);
-	// int i = 0;
-	// while (flag == 0)
-	// {
-	// 	tmp = ft_cut(tmp_buf);
-	// 	line[i] = tmp;
-	// 	tmp_buf = tmp_buf + ft_strlen(line[i]) + 1;
-	// 	i++;
+	// //if (is_meet_n(buf) == 0) 이면 브레이크
+	// tmp = ft_cut(buf);
+	// line[0] = tmp;
+	// tmp_buf = tmp_buf + ft_strlen(line[0] - 3);
+	// tmp = ft_cut(tmp_buf);
+	// line[1] = tmp;
 
-	// }
+	read(fd, buf, BUFFER_SIZE);
+	// printf("%s\n", buf);
+	int i = 0;
+	while (1)
+	{
+		tmp = ft_cut(tmp_buf);
+		line[i] = tmp;
+		if (is_meet_n(tmp_buf) == 0)
+			break;
+		tmp_buf = tmp_buf + ft_strlen(line[i]) + 1;
+		i++;
+	}
+	line[i+1] = 0;
 
+	i = 0;
+	while (line[i])
+	{
+		printf("%d\n", i);
+		printf("%s\n", line[i]);
+		i++;
+	}
 
 
 	free(buf);
@@ -131,7 +167,7 @@ int		main()
 	line = &temp;
 	fd = open("test.txt", O_RDONLY);
 	ret = get_next_line(fd, line);
-	// printf("%s\n", *line);
+	// printf("%s", *line);
 	// while ((ret = get_next_line(fd, line)))
 	// 	printf("%s\n", *line);
 	// printf("%s\n", *line);
