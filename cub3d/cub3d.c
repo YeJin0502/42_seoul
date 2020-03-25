c2adf1dc450b3a8f61e3ee990e4b9b755c5a9468
moongua94 <koom6@naver.com>
2020-03-26 00:58:21 +0900

    day7

--- cub3d/cub3d.c
+++ cub3d/cub3d.c
@@ -6,15 +6,38 @@
 /*   By: gmoon <gmoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/25 23:26:23 by gmoon             #+#    #+#             */
-/*   Updated: 2020/03/26 00:29:19 by gmoon            ###   ########.fr       */
+/*   Updated: 2020/03/26 00:58:06 by gmoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int key_hook(int keycode, void *param)
-{
+// int key_hook(int keycode, void *param)
+// {
+// 	t_info *info;
+
+// 	info = (t_info *)param;
+
 	
+// }
+
+void make_2d_map(t_info *info)
+{
+	int i;
+	int j;
+	int a;
+	int b;
+
+	i = -1;
+	while (++i < info->map_height)
+	{
+		j = -1;
+		while (++j < info->map_width)
+		{
+			a = -1;
+			while (++a < )
+		}
+	}
 }
 
 int main()
@@ -22,6 +45,8 @@ int main()
 	t_info *info;
 	info = make_info(); // 아마 argc, argv를 매개변수로.
 
-	mlx_key_hook(info->win, key_hook, (void *)info); // 아마 나중에 mlx_hook으로 바꿔야 할 듯?
+	make_2d_map(info);
+
+	// mlx_key_hook(info->win, key_hook, (void *)info); // 아마 나중에 mlx_hook으로 바꿔야 할 듯?
 	mlx_loop(info->mlx);
 }
--- cub3d/cub3d.h
+++ cub3d/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: gmoon <gmoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/25 23:26:26 by gmoon             #+#    #+#             */
-/*   Updated: 2020/03/26 00:17:24 by gmoon            ###   ########.fr       */
+/*   Updated: 2020/03/26 00:58:03 by gmoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,21 @@
 typedef struct	s_info
 {
 	int R_width;
-	int R_height;
-	char *NO;
-	char *SO;
-	char *WE;
-	char *EA;
-	int *F[3];
-	int *C[3];
+	int R_height; // x,y 쌍을 다 구조체로 만들까?
+	// char *NO;
+	// char *SO;
+	// char *WE;
+	// char *EA;
+	// int *F[3];
+	// int *C[3]; // 나중에 추가 필요
+	int (*map)[15]; // 원래는 1중 배열로 받아야 할듯? 뒤의 숫자를 모르니까.
+	// 아닌가..? 만들 수 있나? // 괄호 안해주면 안됨
+	int map_width;
+	int map_height;
+	int tile_width;
+	int tile_height;
+	int x;
+	int y;
 	double dir;
 	void *mlx;
 	void *win;
