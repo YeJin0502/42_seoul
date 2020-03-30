4cacb01c4ca2329b2977fee1a142a55d222d41a9
moon9ua <koom6@naver.com>
2020-03-31 07:52:55 +0900

    day12: 24bit bmp

--- cub3d/save_bmp.c
+++ cub3d/save_bmp.c
@@ -6,45 +6,58 @@
 /*   By: gmoon <gmoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/31 03:45:54 by gmoon             #+#    #+#             */
-/*   Updated: 2020/03/31 07:11:14 by gmoon            ###   ########.fr       */
+/*   Updated: 2020/03/31 07:51:57 by gmoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void save_bmp(int fd, t_img *scene)
+void write_bmp_header(int fd, t_img *scene)
 {
     unsigned char bmp_header[54];
     int filesize;
 
-    filesize = 54 + (scene->width * scene->height * 4);
-    ft_memset(bmp_header, 0, 54); // 맞나?
+    filesize = 54 + scene->width * scene->height * 3;
+    ft_memset(bmp_header, 0, 54);
     bmp_header[0] = (unsigned char)'B';
     bmp_header[1] = (unsigned char)'M';
     ft_memcpy(bmp_header + 2, &filesize, sizeof(int));
-    // printf("%d, %d, %d\n", bmp_header[2], bmp_header[3], bmp_header[4]);
-    bmp_header[10] = (unsigned char)54; // 이거도 unsigned char을 붙여줘야 하나?)
+    bmp_header[10] = (unsigned char)54;
     bmp_header[14] = (unsigned char)40;
     ft_memcpy(bmp_header + 18, &scene->width, sizeof(int));
     ft_memcpy(bmp_header + 22, &scene->height, sizeof(int));
     bmp_header[26] = (unsigned char)1;
-    bmp_header[28] = (unsigned char)32; // 24로 많이 하던데
-    // 왜 이 아래는 안쓰지?
-
-    // unsigned char *pixel_data;
-    // pixel_data = (unsigned char *)malloc(filesize - 54);
-    // int i;
-    // i = -1;
-    // while (++i < scene->width * scene->height)
-    // {
-    //     pixel_data[3 * i] = scene->image_data[4 * i + 2];
-    //     pixel_data[3 * i + 1] = scene->image_data[4 * i + 1];
-    //     pixel_data[3 * i + 2] = scene->image_data[4 * i]; // 순서가 맞나?
-    // }
-    
-    unsigned char *bmp;
-
+    bmp_header[28] = (unsigned char)24;
     write(fd, bmp_header, 54);
-    write(fd, scene->image_data, scene->width * scene->height * 4);
-    // write(fd, pixel_data, filesize - 54);
+}
+
+void write_pixel_data(int fd, t_img *scene)
+{
+    int w;
+    int h;
+    unsigned char *pixel_data;
+    int i;
+    int j;
+
+    w = scene->width;
+    h = scene->height;
+    pixel_data = (unsigned char *)malloc(w * h * 3);
+    i = -1;
+    while (++i < scene->width)
+    {
+        j = -1;
+        while (++j < scene->height)
+        {
+            pixel_data[3 * i + 3 * w * j] = scene->image_data[4 * i + 4 * w * (h - j)];
+            pixel_data[3 * i + 3 * w * j + 1] = scene->image_data[4 * i + 4 * w * (h - j) + 1];
+            pixel_data[3 * i + 3 * w * j + 2] = scene->image_data[4 * i + 4 * w * (h - j) + 2];
+        }
+    }
+    write(fd, pixel_data, w * h * 3);
+}
+
+void save_bmp(int fd, t_img *scene)
+{
+    write_bmp_header(fd, scene);
+    write_pixel_data(fd, scene);    
 }
