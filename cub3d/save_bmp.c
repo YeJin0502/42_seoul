3e3001b47c834c6f0d770a0607cc8c04d34cc525
moongua94 <koom6@naver.com>
2020-03-31 04:29:09 +0900

    day12: bmp...

--- cub3d/save_bmp.c
+++ cub3d/save_bmp.c
@@ -6,7 +6,7 @@
 /*   By: gmoon <gmoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/31 03:45:54 by gmoon             #+#    #+#             */
-/*   Updated: 2020/03/31 04:05:27 by gmoon            ###   ########.fr       */
+/*   Updated: 2020/03/31 04:09:54 by gmoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,10 @@
 void save_bmp(int fd, t_img *scene)
 {
     char bitmapheader[54];
+    int filesize;
 
     bitmapheader[0] = 'B';
     bitmapheader[1] = 'M';
-    
+    filesize = 54 + (scene->width * scene->height * 3);
 
 }
