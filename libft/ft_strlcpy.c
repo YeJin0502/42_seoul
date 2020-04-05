9232eafdcfd36fceb306d98694adc6c8c8b059f7
Guhwa Moon <gmoon@c1r3s5.42seoul.kr>
2020-04-05 11:15:07 +0900

    libft: modify first part

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: gmoon <gmoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/24 16:52:30 by gmoon             #+#    #+#             */
-/*   Updated: 2020/02/24 16:52:30 by gmoon            ###   ########.fr       */
+/*   Updated: 2020/04/05 10:24:00 by gmoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
 	size_t	i;
 
+	if (!dest && !src)
+		return (0);
 	if (size == 0)
 		return (ft_strlen(src));
 	i = 0;
--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,19 +6,22 @@
 /*   By: gmoon <gmoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/24 19:26:28 by gmoon             #+#    #+#             */
-/*   Updated: 2020/04/04 05:56:38 by gmoon            ###   ########.fr       */
+/*   Updated: 2020/04/05 11:13:06 by gmoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strrchr(const char *s, int c)
 {
 	char	*ret;
+	int		tmp;
 
 	if (*s == '\0')
 		return (0);
+	ret = 0;
 	while (*s)
 	{
-		if (*s == c)
+		tmp = *s;
+		if (tmp == c)
 			ret = (char *)s;
 		s++;
 	}
