6b466b2b0c49e9af25f6fdf3e5c7abbd2662986e
moongua94 <koom6@naver.com>
2020-04-06 09:07:26 +0900

    gnl: gnl2 대충.

--- get_next_line2/get_next_line.c
+++ get_next_line2/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: gmoon <gmoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/06 05:15:29 by gmoon             #+#    #+#             */
-/*   Updated: 2020/04/06 07:31:37 by gmoon            ###   ########.fr       */
+/*   Updated: 2020/04/06 09:06:39 by gmoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ int is_contain_lf(char *buf)
 	return (0);
 }
 
-int strlen_until_lf(char *str)
+int strlen_lf(char *str)
 {
 	int i;
 
@@ -52,17 +52,19 @@ char *gnl_strjoin(char *s1, char *s2) // 줄바꿈이나 널문자까지?
 	int i;
 
 	s1_size = (s1) ? gnl_strlen(s1) : 0;
-	s2_size = (s2) ? strlen_until_lf(s2) : 0;
+	s2_size = (s2) ? strlen_lf(s2) : 0;
 	size = s1_size + s2_size;
 	if (!(ret = (char *)malloc(size + 1)))
 		return (0);
 	ret[size] = '\0';
-	i = 0;
+	i = -1;
 	// while (*s1) // 이건 왜 안되는지 잘 모르겠음.
-	while (i < s1_size)
-		*(ret + i++) = *(s1++);
-	while (i - s1_size < s2_size) // 맞나?
+	while (++i < s1_size)
+		*(ret + i) = *(s1 + i);
+	while (i - s1_size < s2_size)
 		*(ret + i++) = *(s2++);
+	if (s1) // s1_size로 하면 널문자 있는 문자열을 free 못함.
+		free(s1);
 	return (ret);
 }
 
@@ -95,58 +97,61 @@ void	*gnl_memmove(void *dest, const void *src, size_t n)
 void pull_buf(char *buf) // 참조 의한 호출, 값 의한 호출 헷갈린다.
 {
 	gnl_memmove(buf,
-				buf + strlen_until_lf(buf) + 1,
-				gnl_strlen(buf) - strlen_until_lf(buf));
+				buf + strlen_lf(buf) + 1,
+				gnl_strlen(buf) - strlen_lf(buf));
 	// +1인가? 헷갈리네.
 	// 포인터를 당길 필요가 있나? 그냥 포인터 옮기면 free를 못해줘서 그런가?
 }
 
+int is_line_made(char *buf, char **line)
+{
+	*line = gnl_strjoin(*line, buf);
+	if (is_contain_lf(buf) == 1)
+	{
+		pull_buf(buf);
+		return (1);
+	}
+	return (0);
+}
+
 int get_next_line(int fd, char **line)
 {
-	int ret;
-	static char *buf; // 여러줄 하려면 static으로 바꿔야할걸.
+	static char	*buf;
+	int			ret;
 
-	*line = 0; // line 헷갈리네...
+	*line = 0;
 	if (buf)
 	{
-		*line = gnl_strjoin(*line, buf);
-		if (is_contain_lf(buf) == 1)
-		{
-			pull_buf(buf);
+		if (is_line_made(buf, line) == 1)
 			return (1);
-		}
-		// if (gnl_strlen(buf) < BUFFER_SIZE)
-		// 	return (0);
 	}
 	else
 		buf = (char *)malloc(BUFFER_SIZE + 1);
 	while ((ret = read(fd, buf, BUFFER_SIZE))) // 형변환 안해줘도 되나?
 	{
 		buf[ret] = '\0';
-		*line = gnl_strjoin(*line, buf);
-		if (is_contain_lf(buf) == 1)
-		{
-			pull_buf(buf);
+		if (is_line_made(buf, line) == 1)
 			return (1);
-		}
-		// if (gnl_strlen(buf) < BUFFER_SIZE)
-		// 	return (0);
 	}
 	if (ret < BUFFER_SIZE)
 		return (0);
 	return (-1);
 }
 
-int main()
-{
-	// printf("%s\n", gnl_strjoin("hello", "world")); //buffer_size = 5 라고 가정.
-	// printf("%s\n", gnl_strjoin("hello", "wo\nrl"));
+// int main()
+// {
+// 	// printf("%s\n", gnl_strjoin("hello", "world")); //buffer_size = 5 라고 가정.
+// 	// printf("%s\n", gnl_strjoin("hello", "wo\nrl"));
 
-	int fd;
-	char *line;
+// 	int fd;
+// 	char *line;
 
-	fd = open("test", O_RDONLY);
-	printf("%d [%s]\n", get_next_line(fd, &line), line);
-	printf("%d [%s]\n", get_next_line(fd, &line), line);
-	printf("%d [%s]\n", get_next_line(fd, &line), line);
-}
+// 	fd = open("test", O_RDONLY);
+// 	printf("%d [%s]\n", get_next_line(fd, &line), line);
+// 	free(line);
+// 	printf("%d [%s]\n", get_next_line(fd, &line), line);
+// 	free(line);
+// 	printf("%d [%s]\n", get_next_line(fd, &line), line);
+// 	free(line);
+// 	printf("===\n");
+// }
