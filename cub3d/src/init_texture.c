e2beb98b12b684495bca6e17c6783fb45f136148
moon9ua <koom6@naver.com>
2020-04-17 00:17:38 +0900

    .

--- cub3d/src/init_texture.c
+++ cub3d/src/init_texture.c
@@ -6,7 +6,7 @@
 /*   By: gmoon <gmoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/30 06:29:44 by gmoon             #+#    #+#             */
-/*   Updated: 2020/04/16 23:59:09 by gmoon            ###   ########.fr       */
+/*   Updated: 2020/04/17 00:17:22 by gmoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,7 +77,7 @@ void			init_fc(char *line, char *fc, t_info *info, t_ps *ps)
 	}
 }
 
-static t_img	*make_xpm_img(char *filename, t_info *info) // 다른데서도 쓸만함
+static t_img	*make_xpm_img(char *filename, t_info *info)
 {
 	t_img *img;
 
