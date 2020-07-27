a95395db20f3fc67e82802b4530ed045112d4b31
moon9ua <koom6@naver.com>
2020-07-27 13:01:59 +0900

    .

--- ft_services/setup.sh
+++ ft_services/setup.sh
@@ -4,7 +4,7 @@ clear
 minikube start --vm-driver=virtualbox --extra-config=apiserver.service-node-port-range=1-35000
 
 eval $(minikube docker-env)
-# export ip=$(minikube ip)
+export ip=$(minikube ip)
 
 kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.8.1/manifests/metallb.yaml
 kubectl apply -f srcs/yaml/metallb.yaml
