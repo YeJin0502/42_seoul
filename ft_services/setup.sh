f74f7c4acd51be9ad037d77333576da194dd6175
moon9ua <koom6@naver.com>
2020-07-27 13:40:01 +0900

    .

--- ft_services/setup.sh
+++ ft_services/setup.sh
@@ -10,8 +10,8 @@ kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.8.1/manifes
 kubectl apply -f srcs/yaml/metallb.yaml
 
 docker build -t nginx-image srcs/nginx
-# docker build -t ftps-image srcs/ftps --build-arg ip=$ip
-docker build -t ftps-image srcs/ftps
+docker build -t ftps-image srcs/ftps --build-arg ip=$ip
+# docker build -t ftps-image srcs/ftps
 
 kubectl create -f srcs/yaml/nginx.yaml
 kubectl create -f srcs/yaml/ftps.yaml
