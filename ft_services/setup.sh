e736c03ffd731bcbe537428ac12f6a98b5f639aa
moon9ua <koom6@naver.com>
2020-07-27 11:41:18 +0900

    .

--- ft_services/setup.sh
+++ ft_services/setup.sh
@@ -7,7 +7,7 @@ eval $(minikube docker-env)
 # export ip=$(minikube ip)
 
 kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.8.1/manifests/metallb.yaml
-kubectl create -f srcs/metallb.yaml
+kubectl apply -f srcs/yaml/metallb.yaml
 
 docker build -t nginx-image srcs/nginx
 docker build -t ftps-image srcs/ftps
