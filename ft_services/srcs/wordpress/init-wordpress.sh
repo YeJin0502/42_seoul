e3c0f598147796031ac510461c75509199826cca
moon9ua <koom6@naver.com>
2020-07-29 21:25:06 +0900

    .

--- ft_services/srcs/wordpress/init-wordpress.sh
+++ ft_services/srcs/wordpress/init-wordpress.sh
@@ -1,8 +1,2 @@
-sleep 5
+sleep 3
 mysql --host=mysql-service --user=wordpress_user --password=password wordpress < /tmp/wordpress.sql
-# until [ $? != 1 ]
-# do
-# 	echo "데이터베이스에 wordpress.sql을 넣지 못했습니다. 다시 시도합니다 ...."
-# 	sleep 1
-# 	mysql --host=mysql-service --user=wordpress_user --password=password wordpress < /tmp/wordpress.sql
-# done
