54a72563b8ed2beb59c3959e34f564abbd1c3ff4
moon9ua <koom6@naver.com>
2020-07-29 22:44:17 +0900

    .

--- ft_services/srcs/mysql/entrypoint.sh
+++ ft_services/srcs/mysql/entrypoint.sh
@@ -2,7 +2,7 @@
 # mysqld --user=root --bootstrap < /tmp/mysql-init
 # mysqld --user=root
 
-nohup sh /tmp/init_db.sh &
+nohup sh /tmp/init-db.sh &
 
 mysql_install_db --user=root --datadir="/var/lib/mysql"
 mysqld --datadir="/var/lib/mysql"
