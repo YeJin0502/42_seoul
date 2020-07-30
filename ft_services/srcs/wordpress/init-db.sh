a29892df9437cc06887968aa34571b582589dc65
moon9ua <koom6@naver.com>
2020-07-30 13:50:55 +0900

    .

--- ft_services/srcs/wordpress/init-db.sh
+++
@@ -1,7 +0,0 @@
-until mysql
-do
-	echo "NO_UP" > /dev/null
-done
-
-mysql -uroot --skip-password < init-mysql
-mysql wordpress -uroot --password= < /tmp/wordpress.sql
--- ft_services/srcs/wordpress/init_wordpress.sh
+++ ft_services/srcs/wordpress/init_wordpress.sh
@@ -1,2 +1,7 @@
 sleep 3
-mysql --host=mysql-service --user=wordpress_user --password=password wordpress < /tmp/wordpress.sql
+mysql --host='mysql-service' --user='wordpress_user' --password='password' wordpress < /tmp/wordpress.sql
+until [ $? != 1 ]
+do
+	sleep 1
+	mysql --host='mysql-service' --user='wordpress_user' --password='password' wordpress < /tmp/wordpress.sql
+done
