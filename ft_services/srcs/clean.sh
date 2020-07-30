4e871fb8bb1e1bf072fa30a9dafca79f1c131e7c
moon9ua <koom6@naver.com>
2020-07-30 12:07:52 +0900

    .

--- ft_services/srcs/clean.sh
+++ ft_services/srcs/clean.sh
@@ -3,6 +3,7 @@ kubectl delete pod --all
 kubectl delete service --all
 # kubectl delete persistentvolumeclaims --all
 kubectl delete pvc --all
+kubectl delete configmap --all
 
 # kubectl delete persistentvolume --all
 # kubectl delete pv --all
