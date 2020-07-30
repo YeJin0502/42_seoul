b2dc7c93c78fc5d8a1206738d0ab66d34dffe1af
moon9ua <koom6@naver.com>
2020-07-30 17:56:49 +0900

    .

--- ft_services/srcs/mysql/init_wordpress.sh
+++ ft_services/srcs/mysql/init_wordpress.sh
@@ -3,5 +3,13 @@ do
     echo "no_up"
 done
 
-mysql -u root < /tmp/init_mysql
-mysql -u root --password= wordpress < /tmp/wordpress.sql
+# mysql -u root < /tmp/init_mysql
+# mysql -u root --password= wordpress < /tmp/wordpress.sql
+
+echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password
+echo "CREATE USER 'wordpress_user'@'%' IDENTIFIED BY 'password';" | mysql -u root --skip-password
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress_user'@'%' WITH GRANT OPTION;" | mysql -u root --skip-password
+# echo "update mysql.user set plugin='mysql_native_password' where user='root';" | mysql -u root --skip-password
+# echo "DROP DATABASE test" | mysql -u root --skip-password
+echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
+mysql wordpress -u root --password=  < wordpress.sql
