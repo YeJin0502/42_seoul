ae0fedf5e1883ddfcbaea28b096b623ce34dcb02
moon9ua <koom6@naver.com>
2020-07-30 14:59:33 +0900

    .

--- ft_services/srcs/mysql/entrypoint.sh
+++ ft_services/srcs/mysql/entrypoint.sh
@@ -1,8 +1,8 @@
 mysql_install_db --user=root
 # mysqld --user=root --bootstrap < /tmp/init_mysql
-mysql -uroot < /tmp/init_mysql
-mysql wordpress < /tmp/wordpress.sql
-mysqld --user=root
+mysql -u root < /tmp/init_mysql
+mysql -u wordpress_user -p password wordpress < /tmp/wordpress.sql
+mysqld
 
 # nohup sh /tmp/init-db.sh &
 # mysql_install_db --user=mysql --datadir="/var/lib/mysql"
