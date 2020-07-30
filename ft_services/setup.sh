43c6fa66d7bf7cdb6980f3fe38ab6572777c938e
moon9ua <koom6@naver.com>
2020-07-30 11:57:03 +0900

    .

--- ft_services/setup.sh
+++ ft_services/setup.sh
@@ -6,6 +6,7 @@ minikube start --vm-driver=virtualbox --extra-config=apiserver.service-node-port
 eval $(minikube docker-env)
 
 kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.8.1/manifests/metallb.yaml
+kubectl apply -f srcs/yaml/metallb.yaml
 
 # export ip=$(minikube ip)
 # ip=$(minikube ip)
@@ -16,11 +17,10 @@ kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.8.1/manifes
 # docker build -t wordpress-image srcs/wordpress --build-arg ip=$ip
 sh srcs/build_image.sh
 
-# kubectl create -f srcs/yaml/nginx.yaml
-# kubectl create -f srcs/yaml/ftps.yaml
-# kubectl create -f srcs/yaml/mysql.yaml
-# kubectl create -f srcs/yaml/phpmyadmin.yaml
-# kubectl create -f srcs/yaml/wordpress.yaml
+kubectl create -f srcs/yaml/nginx.yaml
+kubectl create -f srcs/yaml/ftps.yaml
+kubectl create -f srcs/yaml/mysql.yaml
+kubectl create -f srcs/yaml/phpmyadmin.yaml
+kubectl create -f srcs/yaml/wordpress.yaml
 # kubectl create -f srcs/yaml
-kubectl apply -f srcs/yaml
 # 순서가 유의한가?
