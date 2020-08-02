7e22504e70bb7258a580e21b87003e5871a68f95
moon9ua <koom6@naver.com>
2020-08-02 18:31:17 +0900

    .

--- ft_services/setup.sh
+++ ft_services/setup.sh
@@ -13,7 +13,7 @@ docker build -t ftps-image srcs/ftps --build-arg ip=$ip
 docker build -t mysql-image srcs/mysql --build-arg ip=$ip
 docker build -t phpmyadmin-image srcs/phpmyadmin
 docker build -t wordpress-image srcs/wordpress --build-arg ip=$ip
-docker build -t influxdb-image srcs/influxdb
+docker build -t influxdb-image srcs/influxdb --build-arg ip=$ip
 docker build -t grafana-image srcs/grafana --build-arg ip=$ip
 
 # sh srcs/build_image.sh
