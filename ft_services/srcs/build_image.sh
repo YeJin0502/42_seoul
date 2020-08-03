cf0c87d7595575176cc216a5febf93c15ded47f4
moon9ua <koom6@naver.com>
2020-08-03 19:53:15 +0900

    20.08.03 ft_services 제출

--- ft_services/srcs/build_image.sh
+++
@@ -1,9 +0,0 @@
-ip=$(minikube ip)
-docker build -t nginx-image srcs/nginx
-docker build -t ftps-image srcs/ftps --build-arg ip=$ip
-docker build -t mysql-image srcs/mysql --build-arg ip=$ip
-docker build -t phpmyadmin-image srcs/phpmyadmin
-docker build -t wordpress-image srcs/wordpress --build-arg ip=$ip
-docker build -t influxdb-image srcs/influxdb --build-arg ip=$ip
-docker build -t telegraf-image srcs/telegraf --build-arg ip=$ip
-docker build -t grafana-image srcs/grafana --build-arg ip=$ip
