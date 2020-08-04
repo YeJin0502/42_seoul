fc039e53e858bc28410660afc8420041ec5a33ed
moon9ua <koom6@naver.com>
2020-08-04 19:37:45 +0900

    20.08.04 cpp00 : subject 번역

--- cpp/cpp00/ex01/phonebook.cpp
+++ cpp/cpp00/ex01/phonebook.cpp
@@ -3,16 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   phonebook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gmoon <gmoon@student.42.fr>                +#+  +:+       +#+        */
+/*   By: gmoon <gmoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/04 16:30:38 by gmoon             #+#    #+#             */
-/*   Updated: 2020/08/04 16:31:39 by gmoon            ###   ########.fr       */
+/*   Updated: 2020/08/04 19:21:15 by gmoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-class phonebook
+class contact
 {
-    
+    private:
+        char *first;
+        char *last;
+        char *nickname;
+        char *login;
+        char *postal;
+        char *email;
+        char *phone;
+        char *birthday;
+        char *favorite;
+        char *underwear;
+        char *darkest;
+    public:
+        void add()
+        {
+            ;
+        }
+        int exit()
+        {
+            return (0);
+        }
 };
