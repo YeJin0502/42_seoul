44ee42861e25bb2b9146609502780b5995678e8d
moon9ua <gmoon@student.42seoul.kr>
2020-08-04 16:24:54 +0900

    20.08.04 cpp_module_00 start

--- cpp_module/cpp_module_00/README.md
+++ cpp_module/cpp_module_00/README.md
@@ -1,3 +1,13 @@
 # cpp_module_00
 
-* https://modoocode.com/135
+\# cpp
+* https://modoocode.com/135 : 모두의코드
+* https://boycoding.tistory.com/135?category=1006674 : 소년코딩
+* https://blog.hexabrain.net/167
+
+\# 레퍼런스 \# 참조자
+* https://modoocode.com/141
+
+\# 객체 \# 클래스 \# 인스턴스
+* https://modoocode.com/172
+* https://andrew0409.tistory.com/8
--- cpp_module/cpp_module_00/ex01/main.cpp
+++ cpp_module/cpp_module_00/ex01/main.cpp
@@ -1,28 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gmoon <gmoon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/08/04 14:23:00 by gmoon             #+#    #+#             */
-/*   Updated: 2020/08/04 15:55:52 by gmoon            ###   ########.fr       */
+/*   Created: 2020/08/04 16:01:54 by gmoon             #+#    #+#             */
+/*   Updated: 2020/08/04 16:12:32 by gmoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int main(int argc, char **argv)
+int main()
 {
-    if (argc == 1)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-    else
-    {
-        for (int i = 1; i < argc; i++)
-        {
-            for (int j = 0; argv[i][j]; j++)
-                std::cout << char(toupper(argv[i][j]));
-        }
-        std::cout << "\n";
-    }
+    
 }
